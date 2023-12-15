3e419dd009a7f546c97e5d57fff9b0f689fb822e
Allison Truhlar <114596142+allison-truhlar@users.noreply.github.com>
2023-12-15 16:33:51 -0500

    Update tailwind.config.cjs
    Added a hero background image

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -16,6 +16,9 @@ module.exports = {
         serif: ['var(--aw-font-serif)', ...defaultTheme.fontFamily.serif],
         heading: ['var(--aw-font-heading)', ...defaultTheme.fontFamily.sans],
       },
+      backgroundImage: {
+        'hero-pattern':"url('/src/assets/images/hero.jpg')"
+      }
     },
   },
   plugins: [require('@tailwindcss/typography')],
