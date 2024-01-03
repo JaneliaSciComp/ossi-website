a1124eede315433eb0607c1d7edced0eb79fa753
Allison Truhlar <114596142+allison-truhlar@users.noreply.github.com>
2024-01-03 16:34:31 -0500

    Update config.ts
    Added a trial tagsCollection of data "type" and then reference this collection in the projectsCollection

--- astro-tutorial/src/content/config.ts
+++ astro-tutorial/src/content/config.ts
@@ -1,5 +1,5 @@
 // Import utilities from `astro:content`
-import { z, defineCollection } from "astro:content";
+import { z, defineCollection, reference } from "astro:content";
 // Define a `type` (content or data) and `schema` for each collection
 const projectsCollection = defineCollection({
     type: 'content',
@@ -11,10 +11,21 @@ const projectsCollection = defineCollection({
         url: z.string(),
         alt: z.string()
       }),
-      tags: z.array(z.string())
+      tags: reference("tags")
     })
 });
+
+const tagsCollection = defineCollection ({
+    type: 'data',
+    schema: z.object({
+        lab: z.array(z.string()),
+        domain: z.array(z.string()),
+        organism: z.array(z.string())
+    })
+})
+
 // Export a single `collections` object to register your collection(s)
 export const collections = {
   projects: projectsCollection,
+  tags: tagsCollection
 };
