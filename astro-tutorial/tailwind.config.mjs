f2cb3f1d34789067e1cdc8ec3bd97c253de29551
Allison Truhlar <114596142+allison-truhlar@users.noreply.github.com>
2024-01-02 20:52:59 -0500

    Changed tailwind configurations

--- astro-tutorial/tailwind.config.mjs
+++ astro-tutorial/tailwind.config.mjs
@@ -6,16 +6,16 @@ export default {
 	theme: {
 		extend: {
 		  colors: {
-			primary: 'var(--aw-color-primary)',
-			secondary: 'var(--aw-color-secondary)',
-			accent: 'var(--aw-color-accent)',
-			default: 'var(--aw-color-text-default)',
-			muted: 'var(--aw-color-text-muted)',
+			primary: 'var(--color-primary)',
+        secondary: 'var(--color-secondary)',
+        accent: 'var(--color-accent)',
+        default: 'var(--color-text-default)',
+        muted: 'var(--color-text-muted)',
 		  },
 		  fontFamily: {
-			sans: ['var(--aw-font-sans)', ...defaultTheme.fontFamily.sans],
-			serif: ['var(--aw-font-serif)', ...defaultTheme.fontFamily.serif],
-			heading: ['var(--aw-font-heading)', ...defaultTheme.fontFamily.sans],
+			sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+			serif: [...defaultTheme.fontFamily.serif],
+			heading: [...defaultTheme.fontFamily.sans],
 		  },
 		  backgroundImage: {
 			'hero-pattern':"url('/src/assets/images/hero.jpg')"
@@ -25,3 +25,21 @@ export default {
 	  plugins: [require('@tailwindcss/typography')],
 	  darkMode: 'class',
 }
+
+
+// :root {
+//     --font-sans: 'Inter Variable';
+//     --font-serif: var(--font-sans);
+//     --font-heading: var(--font-sans);
+
+//     --color-primary: rgb(30 64 175);
+//     --color-secondary: rgb(30 58 138);
+//     --color-accent: rgb(109 40 217);
+
+//     --color-text-heading: rgb(0 0 0); 
+//     --color-text-default: rgb(16 16 16);
+//     --color-text-muted: rgb(16 16 16 / 66%);
+//     --color-bg-page: rgb(255 255 255);
+
+//     --color-bg-page-dark: rgb(3 6 32);
+//   }
