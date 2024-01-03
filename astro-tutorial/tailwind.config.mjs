78d5263471369ca897565ce0c9c80b007f33ce3a
Allison Truhlar <114596142+allison-truhlar@users.noreply.github.com>
2024-01-02 19:07:29 -0500

    Update tailwind.config.mjs
    Now matches Astrowind theme's configuration for Tailwind

--- astro-tutorial/tailwind.config.mjs
+++ astro-tutorial/tailwind.config.mjs
@@ -1,8 +1,27 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
-	},
-	plugins: [],
+		extend: {
+		  colors: {
+			primary: 'var(--aw-color-primary)',
+			secondary: 'var(--aw-color-secondary)',
+			accent: 'var(--aw-color-accent)',
+			default: 'var(--aw-color-text-default)',
+			muted: 'var(--aw-color-text-muted)',
+		  },
+		  fontFamily: {
+			sans: ['var(--aw-font-sans)', ...defaultTheme.fontFamily.sans],
+			serif: ['var(--aw-font-serif)', ...defaultTheme.fontFamily.serif],
+			heading: ['var(--aw-font-heading)', ...defaultTheme.fontFamily.sans],
+		  },
+		  backgroundImage: {
+			'hero-pattern':"url('/src/assets/images/hero.jpg')"
+		  }
+		},
+	  },
+	  plugins: [require('@tailwindcss/typography')],
+	  darkMode: 'class',
 }
