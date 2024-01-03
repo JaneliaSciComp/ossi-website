4e33921cde0a9919b8779e4f19aca74e2ff60b97
Allison Truhlar <114596142+allison-truhlar@users.noreply.github.com>
2024-01-03 12:33:41 -0500

    Reset all Tailwind config back to starting point

--- astro-tutorial/tailwind.config.mjs
+++ astro-tutorial/tailwind.config.mjs
@@ -1,45 +1,8 @@
 /** @type {import('tailwindcss').Config} */
-const defaultTheme = require('tailwindcss/defaultTheme');
-
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {
-		  colors: {
-			primary: 'var(--color-primary)',
-        secondary: 'var(--color-secondary)',
-        accent: 'var(--color-accent)',
-        default: 'var(--color-text-default)',
-        muted: 'var(--color-text-muted)',
-		  },
-		  fontFamily: {
-			sans: ['Inter var', ...defaultTheme.fontFamily.sans],
-			serif: [...defaultTheme.fontFamily.serif],
-			heading: [...defaultTheme.fontFamily.sans],
-		  },
-		  backgroundImage: {
-			'hero-pattern':"url('/src/assets/images/hero.jpg')"
-		  }
-		},
-	  },
-	  plugins: [require('@tailwindcss/typography')],
-	  darkMode: 'class',
+		extend: {},
+	},
+	plugins: [],
 }
-
-
-// :root {
-//     --font-sans: 'Inter Variable';
-//     --font-serif: var(--font-sans);
-//     --font-heading: var(--font-sans);
-
-//     --color-primary: rgb(30 64 175);
-//     --color-secondary: rgb(30 58 138);
-//     --color-accent: rgb(109 40 217);
-
-//     --color-text-heading: rgb(0 0 0); 
-//     --color-text-default: rgb(16 16 16);
-//     --color-text-muted: rgb(16 16 16 / 66%);
-//     --color-bg-page: rgb(255 255 255);
-
-//     --color-bg-page-dark: rgb(3 6 32);
-//   }
