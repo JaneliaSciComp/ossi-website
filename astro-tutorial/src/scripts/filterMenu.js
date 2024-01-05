fa62ba222ee73d464b1d299870cd4572ed86afdd
Allison Truhlar <114596142+allison-truhlar@users.noreply.github.com>
2024-01-05 09:47:04 -0500

    Update filterMenu.js
    Refactored entirely

--- astro-tutorial/src/scripts/filterMenu.js
+++ astro-tutorial/src/scripts/filterMenu.js
@@ -1,39 +1,31 @@
-export function toggleOptions(key) {
-    const tagListToToggle = key.currentTarget.getAttribute('data-tag-category').nextElementSibling;
+function toggleOptions(e) {
+    const tagListToToggle = document.getElementById(e.target.id).nextElementSibling;
 
     if (tagListToToggle) {
         tagListToToggle.classList.toggle('hidden');
     }
 }
 
-export function handleOptionClick(projectCards, tag) {
-    
-
-    Array.from(projectCards.children).forEach(card => {
-      const cardTags = card.getAttribute('data-tag-names').split(',');
-
-      if (cardTags.includes(tag)) {
-          card.style.display = 'flex';
-          card.style.transition = 'transform 0.5s';
-      } else if (!cardTags.includes(tag)){
-          card.style.display = 'none';
-        }
-    });
-  }
-
-document.addEventListener('DOMContentLoaded', function () {
+function handleOptionClick(e){
+    const selectedTag = e.target.id
     const projectCards = document.getElementById('projectCards');
-    // Attach event listeners to header and option elements
-    document.querySelectorAll('.filter-menu h3').forEach(header => {
-        header.addEventListener('click', function () {
-          toggleOptions(key);
-        });
+
+    Array.from(projectCards.children).forEach(card => {  
+        if (card.classList.contains(selectedTag)) {
+            card.style.display = 'flex';
+            card.style.transition = 'transform 0.5s';
+        } else if (!card.classList.contains(selectedTag)){
+            card.style.display = 'none';
+          }
       });
 
-      document.querySelectorAll('.filter-menu li').forEach(option => {
-        option.addEventListener('click', function () {
-          handleOptionClick(projectCards, tag);
-        });
-      });
-      
-    })
+}
+
+// Attach event listeners to tag category headers and individual tag options
+document.querySelectorAll('.filter-menu h3').forEach(header => {
+    header.addEventListener('click', toggleOptions(e));
+});
+
+document.querySelectorAll('.filter-menu li').forEach(option => {
+    option.addEventListener('click', handleOptionClick(e))
+});
