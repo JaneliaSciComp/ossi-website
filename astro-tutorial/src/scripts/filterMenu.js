162f79f936913ade5b0bdeaf08315542a7c7f456
Allison Truhlar <114596142+allison-truhlar@users.noreply.github.com>
2024-01-05 11:41:56 -0500

    Update filterMenu.js
    Added resetTags function and event listener

--- astro-tutorial/src/scripts/filterMenu.js
+++ astro-tutorial/src/scripts/filterMenu.js
@@ -1,12 +1,19 @@
-function toggleOptions(e) {
+function toggleTagList(e) {
     const tagListToToggle = document.getElementById(e.target.id).nextElementSibling;
+    const chevronIcon = e.target.querySelector('.chevron');
 
     if (tagListToToggle) {
         tagListToToggle.classList.toggle('hidden');
+        chevronIcon.innerHTML = tagListToToggle.classList.contains('hidden') ? '&#9660;' : '&#9650;';
     }
 }
 
-function handleOptionClick(e){
+function toggleSelected(e) {
+    e.target.classList.toggle('selected');
+    handleOptionClick();
+  }
+
+function handleOptionClick(){
     const projectCards = document.getElementById('projectCards');
     const allSelectedTags = Array.from(document.querySelectorAll('.filter-menu li.selected')).map(tag => tag.id);
 
@@ -23,15 +30,25 @@ function handleOptionClick(e){
 
 }
 
+function resetTags(){
+    const projectCards = document.getElementById('projectCards');
+    const allSelectedMenuItems = Array.from(document.querySelectorAll('.filter-menu li.selected'))
+    console.log("before", allSelectedMenuItems)
+    allSelectedMenuItems.forEach(item => item.classList.remove('selected'));
+    console.log("after", allSelectedMenuItems)
+
+    Array.from(projectCards.children).forEach(card => {
+        card.classList.remove('hidden')
+    })
+}
+
 // Attach event listeners to tag category headers and individual tag options
 document.querySelectorAll('.filter-menu h3').forEach(header => {
-    header.addEventListener('click', toggleOptions);
+    header.addEventListener('click', toggleTagList);
 });
 
 document.querySelectorAll('.filter-menu li').forEach(option => {
-    option.addEventListener('click', (e =>{
-        e.target.classList.toggle('selected')
-        handleOptionClick(e)
-        })
-    )
+    option.addEventListener('click', toggleSelected)
 });
+
+document.getElementById("filter-menu-btn").addEventListener('click', resetTags)
