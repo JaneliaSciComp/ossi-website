00aea01573a7611845de7dd97211259e00b1c667
Allison Truhlar <114596142+allison-truhlar@users.noreply.github.com>
2024-01-08 11:30:46 -0500

    Update tailwind.config.mjs
    Edited to include the colors added by the Astrowind template. Also added a background image for the hero

--- astro-tutorial/tailwind.config.mjs
+++ astro-tutorial/tailwind.config.mjs
@@ -2,7 +2,18 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+			  primary: 'var(--color-primary)',
+			  secondary: 'var(--color-secondary)',
+			  accent: 'var(--color-accent)',
+			  default: 'var(--color-text-default)',
+			  muted: 'var(--color-text-muted)',
+			},
+			backgroundImage: {
+				'hero-pattern':"url('/src/assets/images/hero.jpg')"
+			}
 	},
 	plugins: [],
+}
 }
