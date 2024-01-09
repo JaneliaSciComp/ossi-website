a675c079f2314a3f2c7d81c363f8a906de4eefbd
Allison Truhlar <114596142+allison-truhlar@users.noreply.github.com>
2024-01-09 17:42:24 -0500

    Create ProjectGrid.jsx

---
+++ astro-tutorial/src/components/projects/ProjectGrid.jsx
@@ -0,0 +1,23 @@
+import ProjectCard from "./ProjectCard"
+
+export default function ProjectGrid({selectedTags, allProjects, allProjectTags}){
+  return (
+        <section className="col-start-2 col-span-2 md:grid grid-cols-2 auto-rows-max gap-4" id="projectCards">
+            {allProjects.map((project) => {
+              
+               return (<ProjectCard
+                    key={project.slug} 
+                    url={`/projects/${project.slug}/`} 
+                    title={project.data.title} 
+                    imgUrl={project.data.image.url}
+                    imgAlt={project.data.image.alt}
+                    author={project.data.author}
+                    description={project.data.description}
+                    tagsObj={allProjectTags.find(tag => tag.id === project.data.tags.id).data}
+                    selectedTags={selectedTags}
+                />)
+              }
+            )}
+    </section>
+  )
+}
