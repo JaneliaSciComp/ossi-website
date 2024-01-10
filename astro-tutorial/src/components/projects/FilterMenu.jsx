cefcc6aa0ebddf3111b84964f4f4cc35745638ae
Allison Truhlar <114596142+allison-truhlar@users.noreply.github.com>
2024-01-10 11:08:54 -0500

    Update FilterMenu.jsx
    Added a button at the top and bottom of the filter menu that can also toggle its visibility. Added styling to have the filter menu come in from the side on smaller screens.

--- astro-tutorial/src/components/projects/FilterMenu.jsx
+++ astro-tutorial/src/components/projects/FilterMenu.jsx
@@ -1,7 +1,7 @@
 import { TbChevronUp, TbChevronDown } from "react-icons/tb";
 import {useState} from "react"
 
-export default function FilterMenu({filterMenuVisible, uniqueTags, selectedTags, handleTagSelection, handleReset}){
+export default function FilterMenu({toggleFilterMenu, filterMenuVisible, uniqueTags, selectedTags, handleTagSelection, handleReset}){
   const [categoryVisibility, setCategoryVisibility] = useState(() => {
     const initialVisibility = {};
     Object.keys(uniqueTags).forEach((key) => {
@@ -18,7 +18,13 @@ export default function FilterMenu({filterMenuVisible, uniqueTags, selectedTags,
   };
 
   return (
-    <div className={`${filterMenuVisible ? 'flex' : 'hidden'} col-start-1 col-span-1 md:flex flex-col p-4 border-gray-200 border-2 rounded-lg`}>
+    <div className={`${filterMenuVisible ? 'flex translate-x-0' : 'translate-x-full'} z-50 absolute top-0 right-0 transition-transform duration-500 col-start-1 col-span-1 w-full max-h-screen  md:flex flex-col p-4 bg-white border-gray-200 border-2 rounded-lg`}>
+      <button
+        onClick={()=>toggleFilterMenu()}
+      >
+        X
+      </button>
+      <div className="overflow-y-scroll"> 
       {Object.keys(uniqueTags).map(key => (
         <div 
           className='mb-4'
@@ -48,11 +54,19 @@ export default function FilterMenu({filterMenuVisible, uniqueTags, selectedTags,
           </ul>
         </div>
       ))}
+      </div>
+      
       <button 
         className="bg-blue-500 text-white px-4 py-2 rounded"
         onClick={()=>handleReset()}
       >
           Reset
+      </button>
+      <button 
+        className="bg-blue-500 text-white px-4 py-2 rounded"
+        onClick={()=>toggleFilterMenu()}
+      >
+          Apply filters
       </button>
     </div>
   )
