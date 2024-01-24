0d12e0750f389c745d1f82124f9cf40a2df55164
Allison Truhlar <114596142+allison-truhlar@users.noreply.github.com>
2024-01-24 17:47:59 -0500

    Update githubApiHelper.js

--- astro-tutorial/src/utils/githubApiHelper.js
+++ astro-tutorial/src/utils/githubApiHelper.js
@@ -1,5 +1,6 @@
 import {Octokit} from "@octokit/rest"
-const authToken = import.meta.env.OSSI_SITE_TOKEN || process.env.OSSI_SITE_TOKEN
+const authToken = import.meta.env.OSSI_SITE_TOKEN
+console.log(authToken)
 
 export async function getContributorStats(){
     const octokit = new Octokit({
