448c590dc1c32fa0527c7e2affeca67e55e6ff82
Allison Truhlar <114596142+allison-truhlar@users.noreply.github.com>
2024-01-29 10:02:06 -0500

    Added a console.log to authToken to see what's going on with Vercel 404 error

--- astro-tutorial/src/utils/githubApiHelper.js
+++ astro-tutorial/src/utils/githubApiHelper.js
@@ -1,5 +1,6 @@
 import {Octokit} from "@octokit/rest"
 const authToken = import.meta.env.OSSI_SITE_TOKEN || process.env.OSSI_SITE_TOKEN;
+console.log(authToken)
 const octokit = new Octokit({
     auth: authToken,
     log: console
