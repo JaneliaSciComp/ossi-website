25e9226345f15f40b43002925ee90dc65a5dfa6c
Allison Truhlar <114596142+allison-truhlar@users.noreply.github.com>
2024-01-31 12:43:07 -0500

    Astro configuration includes GitHub pages site and base repo

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://janeliascicomp.github.io',
+  base: '/ossi-website',
   integrations: [tailwind({
     applyBaseStyles: false,
   }), icon(), react()],
