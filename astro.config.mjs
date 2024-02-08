cdc6c2885b578254e6b827a7135d93849cdb4b5a
Allison Truhlar <114596142+allison-truhlar@users.noreply.github.com>
2024-02-08 10:27:23 -0500

    astro.config.mjs updated
    Received this error when deploying the material ui component to GitHub pages:
    Named export 'globalStateClasses' not found. The requested module '@mui/utils/generateUtilityClass/index.js' is a CommonJS module, which may not support all module.exports as named exports.
    CommonJS modules can always be imported via the default export, for example using:
    
    import pkg from '@mui/utils/generateUtilityClass/index.js';
    const { globalStateClasses } = pkg;
    
      Stack trace:
        at ModuleJob._instantiate (node:internal/modules/esm/module_job:132:21)
        at async ModuleLoader.import (node:internal/modules/esm/loader:329:24)
        at async generatePages (file:///home/runner/work/ossi-website/ossi-website/node_modules/astro/dist/core/build/generate.js:164:9)
        at async AstroBuilder.build (file:///home/runner/work/ossi-website/ossi-website/node_modules/astro/dist/core/build/index.js:138:5)
        at async build (file:///home/runner/work/ossi-website/ossi-website/node_modules/astro/dist/core/build/index.js:47:3)
    
    Found this seemingly-related conversation so tried updating the config accordingly:
    https://github.com/vikejs/vike/discussions/872

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,22 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
 import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://janeliascicomp.github.io',
-  base: '/ossi-website',
-  integrations: [tailwind({
-    applyBaseStyles: false,
-  }), icon(), react()],
+  site: "https://janeliascicomp.github.io",
+  base: "/ossi-website",
+  integrations: [
+    tailwind({
+      applyBaseStyles: false,
+    }),
+    icon(),
+    react(),
+  ],
   vite: {
     ssr: {
-      noExternal: ["react-icons"],
+      noExternal: ["react-icons", "@mui/base"],
     },
   },
 });
