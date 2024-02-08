040d8b7085f4c7d9e76e8a2949cff365bcd8f6e2
Allison Truhlar <114596142+allison-truhlar@users.noreply.github.com>
2024-02-08 10:01:18 -0500

    Badge component added to FilterMenuBtn to display # of selected filters

--- src/components/projects/ToggleFilterMenuBtn.jsx
+++ src/components/projects/ToggleFilterMenuBtn.jsx
@@ -1,16 +1,35 @@
-import {useStore} from '@nanostores/react';
-import { isFilterMenuVisible } from './stores/filterStore';
+import { Badge } from "@mui/base/Badge";
+import { useStore } from "@nanostores/react";
+import { selectedTags } from "./stores/tagsStore";
+import { isFilterMenuVisible } from "./stores/filterStore";
 import { TbAdjustmentsHorizontal } from "react-icons/tb";
 
-export default function ToggleFilterMenuBtn(){
-    const $isFilterMenuVisible = useStore(isFilterMenuVisible)
+export default function ToggleFilterMenuBtn() {
+  const $isFilterMenuVisible = useStore(isFilterMenuVisible);
+  const $selectedTags = useStore(selectedTags);
 
-    return (
-        <button
-            className='md:hidden btn flex gap-2'
-            onClick={() => isFilterMenuVisible.set(!$isFilterMenuVisible)}
-        >
-            <p>Filters</p><TbAdjustmentsHorizontal className="w-5 h-5 inline-block"/> 
-        </button>
-    )
+  const numFilters = $selectedTags.length;
+
+  return (
+    <Badge
+      className="relative"
+      slots={{ root: "button" }}
+      badgeContent={numFilters}
+      slotProps={{
+        badge: {
+          className: `${
+            numFilters === 0 && "hidden"
+          } z-auto absolute top-0 right-0 font-sans w-8 h-8 leading-8 text-white font-xs rounded-full bg-accent text-center translate-x-1/3 -translate-y-1/3 drop-shadow-lg origin-right`,
+        },
+      }}
+    >
+      <button
+        className="md:hidden btn flex gap-2"
+        onClick={() => isFilterMenuVisible.set(!$isFilterMenuVisible)}
+      >
+        <p>Filters</p>
+        <TbAdjustmentsHorizontal className="w-5 h-5 inline-block" />
+      </button>
+    </Badge>
+  );
 }
