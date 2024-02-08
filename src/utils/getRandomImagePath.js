eebb9ddd369389803b228bc003a6d4950363be30
Allison Truhlar <114596142+allison-truhlar@users.noreply.github.com>
2024-02-08 12:43:55 -0500

    getRandomImagePath.js deleted; function now in ProjectCard

--- src/utils/getRandomImagePath.js
+++
@@ -1,6 +0,0 @@
-export function getRandomImagePath(placeholderImages) {
-  const imagePaths = Object.keys(placeholderImages);
-  const randomIndex = Math.floor(Math.random() * imagePaths.length);
-  const randomPath = imagePaths[randomIndex];
-  return randomPath;
-}
