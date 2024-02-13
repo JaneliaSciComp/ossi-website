3cbb05f76a2462228b448da4a336b064ffc7edb0
Allison Truhlar <114596142+allison-truhlar@users.noreply.github.com>
2024-02-13 09:56:50 -0500

    CardsAndFilters refactored with new components, styling

--- src/components/projects/CardsAndFilters.jsx
+++ src/components/projects/CardsAndFilters.jsx
@@ -1,6 +1,9 @@
-import ToggleFilterMenuBtn from "./ToggleFilterMenuBtn.jsx";
 import FilterMenu from "./FilterMenu.jsx";
-import CardDisplay from "./CardDisplay.jsx";
+import ProjectCount from "./ProjectCount.jsx";
+import ContentCard from "../../components/projects/ContentCard.jsx";
+import { extractIndividualProjectTags } from "../../utils/tagManipulation.js";
+// import UnstyledSelectBasic from "./ProjectTypeDropdown.jsx";
+import ProjectTypeSelector from "./ProjectTypeSelector.jsx";
 
 export default function CardsAndFilters({
   uniqueTags,
@@ -9,19 +12,39 @@ export default function CardsAndFilters({
   contentType,
 }) {
   return (
-    <section className="px-6 md:py-12 lg:py-20 mx-auto max-w-6xl">
-      <div className="flex py-6 md:p-0">
-        <ToggleFilterMenuBtn />
-      </div>
-      <section className="md:grid grid-cols-3 gap-4">
+    <section className="px-6 py-6 md:py-12 lg:py-20 mx-auto max-w-6xl md:grid grid-cols-3 gap-4 border-2 border-red-500">
+      <div className="col-start-1 col-span-1 border-2 border-green-500">
         <FilterMenu uniqueTags={uniqueTags} />
-        <CardDisplay
-          key="CardDisplay"
-          allContent={allContent}
-          baseUrl={baseUrl}
-          contentType={contentType}
-        />
-      </section>
+      </div>
+
+      <div className="col-start-2 col-span-2">
+        <ProjectTypeSelector />
+
+        <ProjectCount allContent={allContent} contentType={contentType} />
+
+        <div className=" md:grid grid-cols-2 auto-rows-max gap-4">
+          {allContent.map((content) => {
+            return (
+              <ContentCard
+                key={content.slug}
+                baseUrl={baseUrl}
+                url={`${baseUrl}/${contentType}/${content.slug}/`}
+                title={content.data.title}
+                authors={content.data["author names"]}
+                tagline={content.data.tagline}
+                tagsArray={
+                  contentType === "projects"
+                    ? extractIndividualProjectTags(content.data)
+                    : content.data.tagsArray
+                }
+                imageSrc={content.data["image file"]}
+                imageAlt={content.data["image alt text"]}
+                projectType={content.data["project type"][0]}
+              />
+            );
+          })}
+        </div>
+      </div>
     </section>
   );
 }
