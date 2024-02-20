ad456a660fce105a33aabb2ec00fac01e8e1618e
Allison Truhlar <114596142+allison-truhlar@users.noreply.github.com>
2024-02-20 14:16:15 -0500

    Hero.jsx randomly loads an image on each page refresh

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,28 @@
-export default function Hero() {
+import { useEffect, useState } from "react";
+import { getRandomImage } from "../utils/getRandomImage";
+
+const heroImages = [
+  "ExM-data-VVD-Viewer-1.png",
+  "ExM-data-VVD-Viewer-2.png",
+  "manc-dorsal-neuVid.png",
+  "Mouse-brain-neuVid.png",
+  "neuVid.png",
+];
+
+export default function Hero({ baseUrl, title, subtitle }) {
+  const [backgroundImg, setBackgroundImg] = useState("");
+  useEffect(() => {
+    backgroundImg === "" && setBackgroundImg(getRandomImage(heroImages));
+  }, []);
+  console.log(backgroundImg);
+
   return (
-    <section className="relative md:-mt-[76px] not-prose">
+    <section
+      className="bg-cover bg-no-repeat relative md:-mt-[76px] not-prose"
+      style={{
+        backgroundImage: `url('${baseUrl}/hero-images/${backgroundImg}')`,
+      }}
+    >
       <div
         className="absolute inset-0 pointer-events-none"
         aria-hidden="true"
@@ -10,36 +32,17 @@ export default function Hero() {
         <div className="py-12 md:py-20">
           <div className="pb-10 md:pb-16 max-w-5xl">
             {title && (
-              <h1
-                className="text-5xl font-bold leading-tighter tracking-tighter mb-4 font-heading text-gray-200"
-                set:html={title}
-              />
+              <h1 className="text-5xl font-bold leading-tighter tracking-tighter mb-4 font-heading text-gray-200">
+                {title}
+              </h1>
             )}
             <div className="max-w-3xl">
               {subtitle && (
-                <p
-                  className="text-xl text-white mb-6 dark:text-slate-300"
-                  set:html={subtitle}
-                />
-              )}
-              {actions && (
-                <div className="max-w-xs sm:max-w-md m-auto flex flex-nowrap flex-col sm:flex-row sm:justify-center gap-4">
-                  {Array.isArray(actions) ? (
-                    actions.map((action) => (
-                      <div className="flex w-full sm:w-auto">
-                        <Button
-                          {...(action || {})}
-                          className="w-full sm:mb-0"
-                        />
-                      </div>
-                    ))
-                  ) : (
-                    <Fragment set:html={actions} />
-                  )}
-                </div>
+                <p className="text-xl text-white mb-6 dark:text-slate-300">
+                  {subtitle}
+                </p>
               )}
             </div>
-            {content && <Fragment set:html={content} />}
           </div>
         </div>
       </div>
