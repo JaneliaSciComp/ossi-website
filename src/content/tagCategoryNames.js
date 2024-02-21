0c579ca9deda49b1ead838664f84af562a78cd67
Allison Truhlar <114596142+allison-truhlar@users.noreply.github.com>
2024-02-21 10:29:19 -0500

    tagCategoryNames moved from src/data to src/content, and updated to include tag colors
    I thought it would be easier to remember to maintain an up-to-date list if it was co-located with content/config.ts - the schema for the content frontmatter, including tags.

---
+++ src/content/tagCategoryNames.js
@@ -0,0 +1,40 @@
+export const tagKeyNames = [
+  { key: "associated labs and projects", color: "var(--color-primary)" },
+  {
+    key: "programming language",
+    color: "var(--color-secondary)",
+  },
+  {
+    key: "software ecosystem",
+    color: "var(--color-accent)",
+  },
+  {
+    key: "related experimental techniques",
+    color: "#8b8883",
+  },
+  {
+    key: "supported file types",
+    color: "#24a3cc",
+  },
+  {
+    key: "open source license",
+    color: "#5084ac",
+  },
+  {
+    key: "software type",
+    color: "#7b0249",
+  },
+  {
+    key: "software use case",
+    color: "#b258b2",
+  },
+  {
+    key: "usage environment",
+    color: "#ba5915",
+  },
+];
+
+// Additional colors to use
+// #39939b
+// #ab3f32
+// #6ebebd
