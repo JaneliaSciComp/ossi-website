ccd57ba384223224bd61d2e4c09d427329a29e0e
Allison Truhlar <114596142+allison-truhlar@users.noreply.github.com>
2024-03-28 13:15:44 -0400

    style: responsive styling for home page

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -24,8 +24,8 @@ export default function Hero({
         backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url('${baseUrl}/hero-images/${backgroundImg.file}')`,
       }}
     >
-      <div className="flex flex-col 2xl:flex-row justify-around 2xl:items-center h-5/6 2xl:h-auto 2xl:w-11/12">
-        <div className="max-w-5xl px-4 md:px-12 lg:px-20 xl:px-32 2xl:pl-32 2xl:pr-0 2xl:self-start">
+      <div className="flex flex-col 2xl:flex-row justify-between md:justify-around 2xl:items-center h-3/4 md:h-5/6 2xl:h-auto 2xl:w-11/12">
+        <div className="h-auto max-w-5xl px-4 md:px-12 lg:px-20 xl:px-32 2xl:pl-32 2xl:pr-0 2xl:self-start">
           {title && (
             <h1 className="text-5xl lg:text-6xl 2xl:text-7xl font-bold leading-tighter tracking-tighter mb-4 font-heading text-gray-200">
               {title}
@@ -38,11 +38,13 @@ export default function Hero({
             </p>
           )}
         </div>
-        <div className="2xl:bg-slate-600 2xl:bg-opacity-50 2xl:py-20 2xl:mx-20 rounded-2xl">
-          {children}
-        </div>
+        {children && (
+          <div className="bg-blue-50 dark:bg-slate-900 bg-opacity-90 py-12 md:py-6 lg:py-20 w-auto lg:self-start md:mx-12 lg:ml-20 xl:ml-32 2xl:mx-20 md:rounded-2xl">
+            {children}
+          </div>
+        )}
       </div>
-      <p className="text-xs text-white mb-6 dark:text-slate-300 absolute right-6 -bottom-6">
+      <p className="text-xs text-white  dark:text-slate-300 absolute right-6 bottom-0">
         {backgroundImg.alt}
       </p>
     </section>
