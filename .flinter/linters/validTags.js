090362a9e6ad60aea8b7aa6edb0875dbb42c26a1
Allison Truhlar <114596142+allison-truhlar@users.noreply.github.com>
2024-04-03 14:18:23 -0400

    feature: adding frontmatter linting GitHub action
    In progress...

---
+++ .flinter/linters/validTags.js
@@ -0,0 +1,12 @@
+// This must return a boolean value.
+
+/**
+ * @param {{field: string, value?: string}} params
+ * @returns {{result: boolean, error?: string}}
+ */
+export async function run(params) {
+  return {
+    result: true,
+    error: "Error",
+  };
+}
