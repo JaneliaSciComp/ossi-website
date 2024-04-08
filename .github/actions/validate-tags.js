d172bbb677fe8fbc9f5431185baa61e0877a0d6e
Allison Truhlar <114596142+allison-truhlar@users.noreply.github.com>
2024-04-08 12:10:19 -0400

    feat: github action to validate tags in project frontmatter

--- .github/actions/validate-tags.js
+++ .github/actions/validate-tags.js
@@ -1,31 +1,37 @@
-console.log(process.cwd());
-
-import matter from "gray-matter";
+import yaml from "yaml";
 import { readFileSync, writeFileSync } from "fs";
 import validTagsList from "../../.github/actions/validTagsList.json" assert { type: "json" };
+import { parse } from "path";
+// import { report } from "process";
 
-console.log(process.cwd());
 const changedFiles = process.env.CHANGED_FILES.split(" ");
+console.log("changed files: ", changedFiles);
+let invalidFrontmatterFiles = [];
+let invalidTagsFiles = {};
 
-if (!changedFiles.length) {
-  console.log("No MD files changed.");
-} else {
-  let invalidFrontmatterFiles = [];
-  let invalidTagsFiles = {};
+// function to extract only frontmatter from MD content
+// relies on frontmatter being sandwiched between ---
+function extractFrontMatter(content) {
+  const match = content.match(/^---\n([\s\S]+?)\n---/);
+  return match ? match[1] : null;
+}
 
-  // Check if frontmatter is valid and if tags are correct
-  function validateFile(filePath) {
-    const content = readFileSync(filePath, "utf8");
-    const parsed = matter(content);
+// function to process MD file and validate tag values
+function validateFile(filePath) {
+  // step 1 - check for valid frontmatter
+  try {
+    const markdownContent = readFileSync(filePath, "utf8");
+    const frontMatterString = extractFrontMatter(markdownContent);
 
-    if (!parsed.data || Object.keys(parsed.data).length === 0) {
-      // Frontmatter is invalid or empty
-      invalidFrontmatterFiles.push(filePath);
-    } else {
+    if (frontMatterString) {
+      const parsedFrontMatter = yaml.parse(frontMatterString);
+      console.log(parsedFrontMatter);
+
+      // step 2 - if valid frontmatter, validate tags
       let invalidTags = [];
       for (const [key, acceptedValues] of Object.entries(validTagsList)) {
-        if (parsed.data.hasOwnProperty(key)) {
-          const value = parsed.data[key];
+        if (parsedFrontMatter.hasOwnProperty(key)) {
+          const value = parsedFrontMatter[key];
           // Check if the value(s) are within the accepted values
           const valuesToCheck = Array.isArray(value) ? value : [value];
           const invalidValues = valuesToCheck.filter(
@@ -33,7 +39,7 @@ if (!changedFiles.length) {
           );
 
           if (invalidValues.length > 0) {
-            invalidTags.push(`${key}: ${invalidValues.join(", ")}`);
+            invalidTags.push(`**${key}**: ${invalidValues.join(", ")}`);
           }
         }
       }
@@ -42,34 +48,52 @@ if (!changedFiles.length) {
         // If there are invalid tags, save them with the filename
         invalidTagsFiles[filePath] = invalidTags;
       }
+    } else {
+      // No front matter found, log error
+      console.error(`No frontmatter found in ${filePath}`);
+      invalidFrontmatterFiles.push(`**${filePath}**`);
     }
+  } catch (error) {
+    // Handle file read or other errors
+    console.error(`An error occurred processing ${filePath}: ${error.message}`);
+    invalidFrontmatterFiles.push(filePath);
   }
+}
 
+// Entrypoint to this script
+if (!changedFiles.length) {
+  console.log("No MD files changed.");
+} else {
   // Main function
-  changedFiles.forEach((file) => {
+  for (const file of changedFiles) {
     validateFile(file);
-  });
-
-  // Generate markdown report
-  let reportContent = "";
-
-  if (invalidFrontmatterFiles.length > 0) {
-    reportContent += `## ⚠️ Invalid Frontmatter!\n\n**One or more Markdown files are missing frontmatter or have an invalid structure!**\n\nBe sure to check for & fix them!\n\nThe following files had invalid frontmatter:\n- ${invalidFrontmatterFiles.join(
-      "\n- "
-    )}\n\n`;
   }
+}
 
-  if (Object.keys(invalidTagsFiles).length > 0) {
-    reportContent += `## ⚠️ Invalid tags!\n\n**One or more of your committed Markdown files have invalid tag values!**\n\nThe following files had invalid tags:\n`;
-    for (const [file, tags] of Object.entries(invalidTagsFiles)) {
-      reportContent += `- ${file} with invalid tags: ${tags.join("; ")}\n`;
+// Generate markdown report
+let reportContent = "";
+
+if (invalidFrontmatterFiles.length > 0) {
+  reportContent += `## :warning: Invalid Frontmatter!\n\n**One or more of your committed Markdown files are missing frontmatter or have an invalid structure!**\n\nThe following files have invalid frontmatter:\n- ${invalidFrontmatterFiles.join(
+    "\n- "
+  )}\n\n`;
+}
+
+if (Object.keys(invalidTagsFiles).length > 0) {
+  reportContent += `## :warning: Invalid tags!\n\n**One or more of your committed Markdown files have invalid tag values!**\n\nAll tags must match the options [here](https://github.com/JaneliaSciComp/ossi-website/tree/main/.github/actions/validTagsList.json), including exact capitalization and spelling. If any tag categories are empty, you must either leave a space and empty square brackets following the colon, (e.g., \`category name: []\`), or comment out or delete the line in the frontmatter.\n\nThe following files have invalid tags:\n`;
+  for (const [file, tags] of Object.entries(invalidTagsFiles)) {
+    reportContent += `\n**${file}:**\n`;
+    for (const tag of tags) {
+      reportContent += `- ${tag}\n`;
     }
   }
+  reportContent += `\nIf this is a pull request to add a new tag category or value, please ensure your PR includes the "new tags" label and disregard this comment.`;
+}
 
-  if (invalidFrontmatterFiles.length > 0 || invalidTagsFiles.length > 0) {
-    writeFileSync("validation-report.md", reportContent);
-    console.log("Validation report generated.");
-  } else {
-    console.log("No validation issues found.");
-  }
+if (reportContent) {
+  reportContent += `\n\n**Add your corrections by pushing them to the branch from which you originated this pull request.**`;
+  writeFileSync("validation-report.md", reportContent);
+  console.log("Validation report generated.");
+} else {
+  console.log("No validation issues found.");
 }
