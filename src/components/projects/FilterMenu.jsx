ac4f0194bf655c0aec92b554e9b029f036dd0ef9
Allison Truhlar <114596142+allison-truhlar@users.noreply.github.com>
2024-04-08 16:14:44 -0400

    add in capital case for the keys only in the filter menu

--- src/components/projects/FilterMenu.jsx
+++ src/components/projects/FilterMenu.jsx
@@ -75,7 +75,8 @@ export default function FilterMenu({ uniqueTags }) {
               className="cursor-pointer font-bold border-b-2 flex items-center justify-between py-2"
               onClick={() => toggleCategoryVisibility(key)}
             >
-              {key}
+              {/* Capitalize the first letter for display purposes*/}
+              {key.charAt(0).toUpperCase() + key.slice(1)}{" "}
               {categoryVisibility[key] ? <TbMinus /> : <TbPlus />}
             </h3>
             <ul
