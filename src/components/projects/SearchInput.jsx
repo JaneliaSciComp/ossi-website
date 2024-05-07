120b2d0ba32d2ceca4e5eb40dd39031cdbe49205
Allison Truhlar <amtruhlar@gmail.com>
2024-05-07 09:26:51 -0400

    test: explicitly set search input text color different from placeholder

--- src/components/projects/SearchInput.jsx
+++ src/components/projects/SearchInput.jsx
@@ -17,6 +17,7 @@ export default function SearchInput() {
         </button>
       </div>
       <Input
+        className="text-black"
         type="text"
         id="search"
         placeholder="Keyword"
