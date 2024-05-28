5596cfae521053b904cfcafda57d2fb1d850243d
Allison Truhlar <amtruhlar@gmail.com>
2024-05-28 15:51:27 -0400

    style: center content on larger screens, except hero on search pages

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -8,8 +8,8 @@ export default function Hero({
   title,
   subtitle,
   heightClasses,
-  alignmentClasses,
   paddingClasses,
+  captionAlignment,
   children,
 }) {
   const [backgroundImg, setBackgroundImg] = useState("");
@@ -32,26 +32,18 @@ export default function Hero({
   return (
     <section
       className={twMerge(
-        "bg-cover bg-no-repeat bg-center relative flex flex-col justify-end 2xl:justify-center not-prose",
+        "relative bg-cover bg-no-repeat bg-center not-prose",
         heightClasses
       )}
       style={style}
     >
-      <p className="text-xs text-white  dark:text-slate-300 absolute left-6 md:left-12 lg:left-32 xl:left-32 bottom-0 ">
-        {backgroundImg.alt}
-      </p>
       <div
         className={twMerge(
-          "flex flex-col 2xl:flex-row justify-around 2xl:items-center h-5/6 2xl:h-1/2 2xl:w-11/12",
-          alignmentClasses
+          "flex flex-col min-h-full justify-around items-start mx-auto pt-20 max-w-screen-2xl px-6 md:px-20 lg:px-32",
+          paddingClasses
         )}
       >
-        <div
-          className={twMerge(
-            "h-auto max-w-5xl px-4 md:px-12 lg:px-32 2xl:pr-0 2xl:self-start",
-            paddingClasses
-          )}
-        >
+        <div>
           {title && (
             <h1 className="text-5xl lg:text-6xl 2xl:text-7xl font-bold leading-tighter tracking-tighter mb-4 font-heading text-gray-200">
               {title}
@@ -66,11 +58,19 @@ export default function Hero({
         </div>
         {children && (
           <div
-            className={`max-w-3xl bg-blue-50 dark:bg-slate-900 bg-opacity-90 mb-4 md:mb-0 py-6 lg:py-8 w-auto lg:self-start mx-4 md:mx-12 lg:mx-32 lg:mr-0 2xl:ml-20 3xl:mx-32 rounded-2xl`}
+            className={`max-w-3xl bg-blue-50 dark:bg-slate-900 bg-opacity-90 mb-4 md:mb-0 py-6 lg:py-8 w-auto rounded-2xl`}
           >
             {children}
           </div>
         )}
+        <p
+          className={twMerge(
+            "absolute bottom-0 max-auto text-xs text-white dark:text-slate-300",
+            captionAlignment
+          )}
+        >
+          {backgroundImg.alt}
+        </p>
       </div>
     </section>
   );
