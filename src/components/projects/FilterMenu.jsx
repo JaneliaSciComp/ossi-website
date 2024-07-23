8ab814ddd6464b2284a0c69cd1cd27d0fece690b
Allison Truhlar <amtruhlar@gmail.com>
2024-07-23 16:15:06 -0400

    style: add bg color to filter menu pop-out

--- src/components/projects/FilterMenu.jsx
+++ src/components/projects/FilterMenu.jsx
@@ -51,7 +51,7 @@ export default function FilterMenu({ uniqueTags }) {
     <div
       className={`${
         $isFilterMenuVisible ? "flex translate-x-0" : "translate-x-full"
-      }  z-40 md:z-auto fixed md:static  md:translate-x-0 top-0 right-0  transition-transform duration-500 w-full h-[100dvh] md:h-auto md:max-h-full flex-col px-4 md:pl-0 `}
+      }  bg-page z-40 md:z-auto fixed md:static  md:translate-x-0 top-0 right-0  transition-transform duration-500 w-full h-[100dvh] md:h-auto md:max-h-full flex-col px-4 md:pl-0 `}
     >
       <button
         className="md:hidden self-end m-2 btn-secondary rounded-full"
