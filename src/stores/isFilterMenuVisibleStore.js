5aa1739f4fd65e0e10fa2d00b4c1e4e8c08c0463
Allison Truhlar <amtruhlar@gmail.com>
2024-07-31 07:04:54 -0400

    chore: move toggle func to store

--- src/stores/isFilterMenuVisibleStore.js
+++ src/stores/isFilterMenuVisibleStore.js
@@ -1,3 +1,8 @@
 import { atom } from "nanostores";
 
 export const isFilterMenuVisible = atom(false);
+
+export function toggleVisibility() {
+  const prevVisbility = isFilterMenuVisible.get();
+  isFilterMenuVisible.set(!prevVisbility);
+}
